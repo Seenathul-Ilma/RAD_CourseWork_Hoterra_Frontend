8c494a55dd05fc6486a06208831f1024041b374f
ZeenathulIlma <seenathulilma121243@gmail.com>
2025-11-29 22:12:56 +0530

    feat: add Footer component with copyright notice

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+
+export default function Footer() {
+  return (
+    <>
+        <footer className="text-center mt-12 text-gray-500 text-sm">
+            <p>© 2025 Hoterra. All rights reserved.</p>
+        </footer>
+    </>
+  )
+}
