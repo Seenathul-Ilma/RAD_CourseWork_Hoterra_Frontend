a5de6de45a90672a5de12d3ad3ef9efaf011e392
ZeenathulIlma <seenathulilma121243@gmail.com>
2026-01-04 21:12:10 +0530

    Completed Deployment

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from "axios";
 import { refreshAccessToken } from "./auth";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api/v1"
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1"
 })
 
 const PUBLIC_ENDPOINTS = ["/auth/register", "/auth/login"]
